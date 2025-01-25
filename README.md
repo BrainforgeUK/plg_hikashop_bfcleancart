53cef34613ab9e453de3c161e6533b2578116537
Brainforge.UK <brainforge.uk@btinternet.com>
2025-01-25 12:08:32 +0000

    Added cart cleanup and robot checks.

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-This plugin can be used to clean the cart when an order is confirmed.
-
-This can be useful when you have several confirmed / paid order states such as 'Paid by PayPal', 'Paid by Worldpay',
-'Paid by Telephone' etc and not the more usual single 'confirmed' state.
+This plugin performs the following functions:
+<ul>
+<li><b>Cleans the cart when an order is confirmed.</b>
+<br>This can be useful when you have several confirmed / paid order states such as:
+<br>'Paid by PayPal', 'Paid by Worldpay', 'Paid by Telephone' etc and not the more usual single 'confirmed' state.</li>
+<li><b>Prevents robots from creating a cart and adding items to a cart.</b>
+<br>i.e. the #__hikashop_cart table does not fill up with these.</li>
+<li><b>Run Hikashop cart cleanup when an item is added to a cart</b>.
+<br>See Hikashop configuration advanced settings for retention period and check frequency.</li>
+</ul>
--- bfcleancart.php
+++ bfcleancart.php
@@ -1,61 +1,15 @@
 <?php
 /**
- * @package    Hikashop Plugin to clean cart when order completed by BrainforgeUK
+ * @package    Enhanced Hikashop cart cleaner.
  * @author     https://www.brainforge.co.uk
- * @copyright  (C) 2020 Jonathan Brain. All rights reserved.
+ * @copyright  (C) 2020-2025 Jonathan Brain. All rights reserved.
  * @license    GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 
-defined('_JEXEC') or die('Restricted access');
+use Brainforgeuk\Plugin\Hikashop\BfCleanCart\Extension\BfCleanCart;
 
-use Joomla\CMS\Factory;
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Plugin\CMSPlugin;
+\defined('_JEXEC') or die;
 
-class plgHikashopBfcleancart extends CMSPlugin
+class plghikashopbfcleancart extends BfCleanCart
 {
-	/**
-	 * Constructor
-	 *
-	 * @param   object  &$subject  The object to observe
-	 * @param   array   $config    An optional associative array of configuration settings.
-	 *                             Recognized key values include 'name', 'group', 'params', 'language'
-	 *                             (this list is not meant to be comprehensive).
-	 *
-	 * @since   1.5
-	 */
-	public function __construct(&$subject, $config) {
-		parent::__construct($subject, $config);
-
-		$this->app = Factory::getApplication();
-		$language = Factory::getLanguage();
-		$language->load('plg_hikashop_bfcleancart', __DIR__);
-	}
-
-	/**
-	 */
-	public function onAfterOrderCreate(&$order, &$send_email)
-	{
-		$this->cleanCart($order);
-	}
-
-	/**
-	 */
-	public function onAfterOrderUpdate(&$order, &$send_email)
-	{
-		$this->cleanCart($order);
-	}
-
-	/**
-	 */
-	protected function cleanCart(&$order)
-	{
-		$completedOrderStatii = $this->params->get('completedorderstatii');
-		if (!empty($completedOrderStatii) &&
-			in_array($order->order_status, $completedOrderStatii))
-		{
-			$cartClass = hikashop_get('class.cart');
-			$cartClass->cleanCartFromSession();
-		}
-	}
 }
